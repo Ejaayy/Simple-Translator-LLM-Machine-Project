ee38455b059a11e75537b35914cabc7d4fd6a3a9
Edriene Paingan <edriene_paingan@dlsu.edu.ph>
2025-03-04 22:03:53 +0800

    Update1 MP2.h

--- MP2.h
+++ MP2.h
@@ -1,33 +1,33 @@
+#ifndef MP2_H
+#define MP2_H
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
-#define MAX_LETTER 20 // Max length for words
-#define MAX_PAIRS 10  // Max language-translation pairs per entry
-#define MAX_ENTRY 150 // Max dictionary entries
+#define MAX_ENTRY 150
+#define MAX_PAIRS 10
 
-typedef char Str20[MAX_LETTER + 1]; // String type (20 characters + null terminator)
+typedef char Str20[21];
 
-// Structures
-typedef struct translationPairTag
-{ // holds pair for word
-  Str20 lang,
-        trans;
-} translationPairType;
+typedef struct {
+    Str20 lang;
+    Str20 trans;
+} pairType;
 
-typedef struct entryTag
-{ // holds each word
-  translationPairType pair[MAX_PAIRS];
-  int pairCount; // how many translation pair there is in an entry
+typedef struct {
+    pairType pair[MAX_PAIRS];
+    int pairCount;
 } entryType;
 
 char getChoice(char choice);
 void updateLanguageTranslationPair(entryType *entry, Str20 language, Str20 translation);
 void showEntries(entryType entries[], int matchedIndexes[], int matchCount);
-int isDuplicate(entryType entries[], int countEntry, Str20 language, Str20 translate, int matchedIndexes[]); //Can be revised into isDuplicateLangTransPair
+int isDuplicate(entryType entries[], int countEntry, Str20 language, Str20 translate, int matchedIndexes[]);
 void getLanguageTranslation(Str20 language, Str20 translation);
 void addEntry(entryType *entries, int *countEntry);
 void addTranslation(entryType *entries, int *countEntry);
 int displayMainMenu();
 
 #include "MP2.c"
+#endif
