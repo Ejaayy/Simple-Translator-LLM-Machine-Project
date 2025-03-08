5d1efe9766d2dd331d17a23e95420d3398615a7a
Edriene Paingan <edriene_paingan@dlsu.edu.ph>
2025-03-08 21:01:31 +0800

    Update69 MP2.h

--- MP2.h
+++ MP2.h
@@ -9,6 +9,7 @@
 #define MAX_LETTER 20 // Max length for words
 #define MAX_PAIRS 10  // Max language-translation pairs per entry
 #define MAX_ENTRY 150 // Max dictionary entries
+#define MAX_WORDS 50 //gawa gawa ko lang
 
 typedef char Str20[MAX_LETTER + 1]; // String type (20 characters + null terminator)
 
@@ -30,16 +31,27 @@ void toLowerString(char *str);
 void inputWord(char sentence[]);
 void updateLanguageTranslationPair(entryType *entry, Str20 language, Str20 translation);
 void showMatchedEntries(entryType entries[], int matchedIndexes[], int matchCount);
-int isDuplicate(entryType entries[], int countEntry, Str20 language, Str20 translate, int matchedIndexes[]); //Can be revised into isDuplicateLangTransPair
+int isDuplicate(entryType entries[], int countEntry, Str20 language, Str20 translate, int matchedIndexes[], int currentEntryIndex); //Can be revised into isDuplicateLangTransPair
 void getLanguageTranslation(Str20 language, Str20 translation);
+int countEntryIsZero(int countEntry);
 void addEntry(entryType *entries, int *countEntry);
 void addTranslation(entryType *entries, int *countEntry);
-void displayAllEntries(entryType entry[], int countEntry);
+void displayModeAllEntries(entryType entry[], int countEntry);
 void deleteEntry(entryType entries[], int *countEntry);
 void deleteTranslation(entryType *entries, int *countEntry);
 void searchWord(entryType *entries, int countEntry);
 void searchTranslation(entryType *entries, int countEntry);
+void exportFile(entryType *entries, int countEntry);
+void importFile(entryType *entries, int *countEntry);
+int exitFunction();
+int displayManageDataMenu();
+int translateMenu();
 int displayMainMenu();
 
+int filterWords(char *textInput, Str20 filteredWords[]);
+void printFilteredWords(Str20 filteredWords[], int wordCount);
+char* translateWord(entryType entries[], int countEntry, Str20 filteredWord, Str20 sourceLanguage, Str20 outputLanguage);
+void translateTextInput(entryType *entries, int countEntry);
 #include "MP2.c"
+#include "translateFunction.c"
 #endif
